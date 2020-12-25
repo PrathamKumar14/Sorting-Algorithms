c260cb274995952de4b8868f4fb7e2c4a4d718a5
Pratham kumar <prathamsolera@gmail.com>
2020-12-25 12:35:08 +0530

    Add Bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,17 @@
+class BubbleSort {
+	public static void main(String ar[]) {
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length - i - 1; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}		
+		}
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
